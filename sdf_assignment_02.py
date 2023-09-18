b800084cf82461b16e8d66276e7f592a91903933
Shagan sran <Shaganpreetsingh2@academic.rrc.ca>
2023-09-18 11:57:08 -0500

    Added text in the files

---
+++ sdf_assignment_02.py
@@ -0,0 +1,21 @@
+"""
+Description: Created assignment 2
+Author: Shaganpreet singh sran
+Date: September 17, 2023
+Usage: sdf assignment 2
+"""
+
+# Simple data types
+
+# Declare a variable to store a name and initialize it to your first name.
+first_name = "Shagan"
+
+print(f"value: {first_name} type: {type(first_name).name}")
+
+# Declare a variable for Manitoba Driver's License status and initialize it.
+manitoba_license_status = True  
+
+print(f"value: {manitoba_license_status} type: {type(manitoba_license_status).name}")
+
+# Declare a variable to store the current year and initialize it.
+current_year = 2023
