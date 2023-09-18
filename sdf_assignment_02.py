fafc6f558ee1e51223cecc86f8093e729b67d532
Shagan sran <Shaganpreetsingh2@academic.rrc.ca>
2023-09-18 12:01:54 -0500

    changes made

--- sdf_assignment_02.py
+++ sdf_assignment_02.py
@@ -19,3 +19,35 @@ print(f"value: {manitoba_license_status} type: {type(manitoba_license_status).na
 
 # Declare a variable to store the current year and initialize it.
 current_year = 2023
+
+
+print(f"this year: {current_year} type: {type(current_year).name}")
+
+# Add 1 to the year.
+current_year += 1
+
+print(f"next year: {current_year} type: {type(current_year).name}")
+
+# Performing Calculations
+
+# Declare constants for GST and PST rates.
+GST_RATE = 0.08  
+PST_RATE = 0.16  
+
+# Declare a variable for the vehicle price and initialize it.
+vehicle_price = 100000.00  
+
+# Calculate GST and PST taxes.
+gst_tax = vehicle_price * GST_RATE
+pst_tax = vehicle_price * PST_RATE
+
+# Calculate the total cost including taxes.
+total_cost = vehicle_price + gst_tax + pst_tax
+
+print(f"pre-tax value: {vehicle_price} PST: {pst_tax} GST: {gst_tax} total: {total_cost}")
+
+print(f"pre-tax value: ${vehicle_price:.2f} PST: ${pst_tax:.2f} GST: ${gst_tax:.2f} total: ${total_cost:.2f}")
+
+
+
+
